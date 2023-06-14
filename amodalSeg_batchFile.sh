879d24872f8504a0f2e6b12b4c2a011648f6fb74
Jostan86 <jostanbrown86@gmail.com>
2023-06-14 06:55:50 -0700

    update batch file AGAINx2

--- amodalSeg_batchFile.sh
+++ amodalSeg_batchFile.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 0-18:00:00                # time limit for job (HH:MM:SS)
 
 module load python3
-source /nfs/hpc/share/browjost/detr_apple/venv/bin/activate
+source /nfs/hpc/share/browjost/detr_apple/venv_amodal/bin/activate
 
 # run my job (e.g. matlab, python)
 srun --export ALL python3 -m torch.distributed.launch --nproc_per_node=2 --use_env main2.py --masks --lr_drop 15 --coco_path /nfs/hpc/share/browjost/detr_apple/coco_apples/ --batch_size 2 --epochs 25 --output_dir /nfs/hpc/share/browjost/detr_apple/logdirs/amodalSeg --dataset_file coco_apples_amodal --frozen_weights /nfs/hpc/share/browjost/detr_apple/logdirs/amodalFT/checkpoint.pth
