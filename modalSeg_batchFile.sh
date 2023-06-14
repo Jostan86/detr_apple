bf0afb0ccf68154dd68aafd8d49c847ca6a2c39f
Jostan86 <jostanbrown86@gmail.com>
2023-06-14 06:39:01 -0700

    update batch file AGAIN

--- modalSeg_batchFile.sh
+++ modalSeg_batchFile.sh
@@ -10,4 +10,4 @@ module load python3
 source /nfs/hpc/share/browjost/detr_apple/venv/bin/activate
 
 # run my job (e.g. matlab, python)
-srun --export ALL python3 -m torch.distributed.launch --nproc_per_node=2 --use_env main2.py --masks --lr_drop 15 --coco_path /nfs/hpc/share/browjost/detr_apple/coco_apples/ --batch_size 2 --epochs 25 --output_dir /nfs/hpc/share/browjost/detr_apple/logdirs/modalSeg --dataset_file coco_apples_amodal --frozen_weights /nfs/hpc/share/browjost/detr_apple/logdirs/modalFT/checkpoint.pth
+srun --export ALL python3 -m torch.distributed.launch --nproc_per_node=2 --use_env main2.py --masks --lr_drop 15 --coco_path /nfs/hpc/share/browjost/detr_apple/coco_apples/ --batch_size 2 --epochs 25 --output_dir /nfs/hpc/share/browjost/detr_apple/logdirs/modalSeg --dataset_file coco_apples_modal --frozen_weights /nfs/hpc/share/browjost/detr_apple/logdirs/modalFT/checkpoint.pth
